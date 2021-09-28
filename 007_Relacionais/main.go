0c5cae9bf75f4deb5f8efab63a73f12d4852e46d
Felipe Araújo <fplopesaraujo@gmail.com>
2021-09-28 19:15:30 -0300

    Operadores relacionais adicionado

---
+++ 007_Relacionais/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Comparações: ")
+	fmt.Println("Igual:", "Banana" == "Banana")
+	fmt.Println("Diferente:", 3 != 2)
+	fmt.Println("Maior que:", 3 > 2)
+	fmt.Println("Menor que:", 3 < 2)
+	fmt.Println("Maior ou igual a:", 3 >= 2)
+	fmt.Println("Menor ou igual a:", 3 <= 2)
+
+}
