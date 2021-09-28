81ffbd7afba50919cc03a464e0bb325c2310b62d
Felipe Araújo <fplopesaraujo@gmail.com>
2021-09-28 19:04:58 -0300

    Valores zero adicionado

---
+++ 002_Zeros/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+	var e *int
+	fmt.Println("--------------------------------------------------------")
+	fmt.Println("As seguintes variáveis receberam por padrão os valores: ")
+	fmt.Printf("A: %v \n", a)
+	fmt.Printf("B: %v \n", b)
+	fmt.Printf("C: %v \n", c)
+	fmt.Printf("D: %q \n", d)
+	fmt.Printf("E: %v \n", e)
+	fmt.Println("--------------------------------------------------------")
+}
