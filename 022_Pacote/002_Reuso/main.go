d9a101ca075f156d6dbf683654d48972a200d120
Felipe Araújo <fplopesaraujo@gmail.com>
2021-09-30 10:53:44 -0300

    Pacote adicionado

---
+++ 022_Pacote/002_Reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/efipezera/area"
+)
+
+func main() {
+	fmt.Println(area.Circulo(6.0))
+	fmt.Println(area.Retangulo(5.0, 2.0))
+	//fmt.Println(area._TrianguloEquilatero(5.0, 2.0))
+}
